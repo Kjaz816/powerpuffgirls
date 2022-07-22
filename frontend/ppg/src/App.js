9e28b7c124ffbc88ddba5daa7dccbb01d0feafaf
Kjaz816 <79782299+Kjaz816@users.noreply.github.com>
2022-07-23 02:03:44 +1200

    returned to origin

--- frontend/ppg/src/App.js
+++ frontend/ppg/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App"> 
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <meta name="google-signin-client_id" content="777744513405-irfgt3aevra0cb2nre817c7j08uqfr4c.apps.googleusercontent.com"></meta>
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    
     
       Hello World
       
@@ -14,12 +12,7 @@ function App() {
   );
 }
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+
+
 
 export default App;
